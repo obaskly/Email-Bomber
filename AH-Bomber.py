5893a4095487726c24ba61768c6b94f4d866f0b2
Bropocalypse Team <amineninja9@gmail.com>
2021-06-19 16:45:44 +0100

    Update AH-Bomber.py

--- AH-Bomber.py
+++ AH-Bomber.py
@@ -30,7 +30,7 @@ def main():
     l1 = Label(form, text='Your Password', background='black', foreground='white')
     l1.place(x=5, y=40)
 
-    global e0, e1, e2, e4, e5, e6
+    global e0, e1, e2, e4, e5
     e0 = Entry(form, width=20)
     e0.place(x=90, y=10)
 
@@ -62,11 +62,18 @@ def main():
     lab.place(x=85, y=445)
 
     ########### Mass Send
+
+    l4 = Label(form2, text='Amount of\ntimes to send', background='black', foreground='white')
+    l4.place(x=5, y=10)
+
+    e5 = Entry(form2, width=20)
+    e5.place(x=90, y=20)
+
     b2 = Button(form2, text='Load Emails', background='black', foreground='white', command= load)
-    b2.place(x=80, y=30)
+    b2.place(x=80, y=60)
 
     b3 = Button(form2, text='Attack', background='black', foreground='white', command= lambda:masssend(lab, var))
-    b3.place(x=95, y=70)
+    b3.place(x=95, y=90)
 
     mainloop()
 
@@ -129,19 +136,26 @@ def load():
         messagebox.showinfo("", ''.join(f"Number Of Emails Is : {len(memails)}"))
 
 def masssend(lab, var):
-
+    count1 = int(e5.get())
     sender = email
-    recipients = memails
-    msg = MIMEText(generator())
-    msg['Subject'] = generator()
-    msg['From'] = 'sorry :)'
-    msg['To'] = ", ".join(recipients)
-    text = msg.as_string()
+    victims = memails
+    
     try:
-        server.sendmail(sender, recipients, text)
-        var.set(var.get()+len(memails))
-        lab['text'] = 'Emails Sent : ' + str(var.get())
+        lol = 0
+        while lol < count1:
+            lol+=1
+
+            msg = MIMEText(generator())
+            msg['Subject'] = generator()
+            msg['From'] = 'sorry :)'
+            msg['To'] = ", ".join(victims)
+            text = msg.as_string()
+            
+            server.sendmail(sender, victims, text)
+            var.set(var.get()+len(memails))
+            lab['text'] = 'Emails Sent : ' + str(var.get())
         messagebox.showinfo("", ''.join('Done'))
+
     except smtplib.SMTPAuthenticationError:
         messagebox.showerror("Error", ''.join('Incorrect Username Or Password'))
         messagebox.showerror("Error", ''.join('Make Sure The Less Secure Apps Option Is Enabled'))
